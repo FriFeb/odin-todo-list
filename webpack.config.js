1b0dbce3cae415ce820f4f00dd6cec98bc289004
FriFeb <51440204+FriFeb@users.noreply.github.com>
2023-12-21 07:11:29 +0200

    set up project starter

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "bundle.js",
+    path: `${__dirname}/dist`,
+    clean: true,
+  },
+};
