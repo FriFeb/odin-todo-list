2834d6703587b1b42e93df96b01b448ce67ca5e5
FriFeb <51440204+FriFeb@users.noreply.github.com>
2024-02-15 12:15:26 +0200

    feat(DOMtasks): add description and priority fields

--- src/DOM/dynamicElements.js
+++ src/DOM/dynamicElements.js
@@ -1,8 +1,8 @@
 import App from '../services/app';
 
-function createDOMProjectElement(name, id) {
+function createDOMProjectElement(title, id) {
   return `<div id='${id}' class='row my-3'>
-      <div class="col project-name">${name}</div>
+      <div class="col project-title">${title}</div>
       <div class="col-auto read">
         <i class="bi bi-box-arrow-in-right"></i>
       </div>
@@ -14,10 +14,10 @@ function createDOMProjectElement(name, id) {
 }
 
 function getProjectElements() {
-  const projectNames = App.getProjectNames();
+  const projectTitles = App.getProjectTitles();
 
-  return projectNames.map((name, index) =>
-    createDOMProjectElement(name, index)
+  return projectTitles.map((title, index) =>
+    createDOMProjectElement(title, index)
   );
 }
 
@@ -33,11 +33,16 @@ export function renderProjectElements() {
   );
 }
 
-function createDOMTaskElement(name, id) {
-  return `<div id='${id}' class='row my-3'>
-      <div class="col task-name">${name}</div>
-      <div class="col-auto read">
-        <i class="bi bi-box-arrow-in-right"></i>
+function createDOMTaskElement(title, description, priority, id) {
+  return `<div id='${id}' class='row my-3 align-items-center'>
+      <div class="row col">
+        <div class="col task-title">${title}</div>
+        <div class="col-12 task-description" style="font-size: 0.5rem;">
+          ${description}
+        </div>
+      </div>
+      <div class="col-auto task-priority">
+        <i class="bi bi-flag"></i>
       </div>
       <div class="col-auto delete">
         <i class="bi bi-trash3"></i>
@@ -46,9 +51,11 @@ function createDOMTaskElement(name, id) {
 }
 
 function getTaskElements() {
-  const taskNames = App.getCurrentProjectTasksNames();
+  const tasks = App.getCurrentProjectTasks();
 
-  return taskNames.map((name, index) => createDOMTaskElement(name, index));
+  return tasks.map((task, index) =>
+    createDOMTaskElement(task.title, task.description, task.priority, index)
+  );
 }
 
 export function renderTaskElements() {
